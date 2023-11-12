0751eb2b0370493d05531f1e837cfaf64403c7c6
ahmedabdelaziz5 <ahmedabdelaziz6019@gmail.com>
2023-11-12 14:41:26 +0200

    add env to the server

--- app.js
+++ app.js
@@ -12,12 +12,15 @@ app.use(bodyParser.json());
 
 require("dotenv").config();
 
+// database connection 
 const { databaseConnection } = require("./config/db.config");
 databaseConnection();
 
+// firebase connection ( using as a pushing server )
 const { connectToFireBase } = require('./config/fireBase.config');
 connectToFireBase();
 
+// server routes
 app.use(require("./routes/user.route"));
 app.use(require("./routes/match.route"));
 app.use(require("./routes/recommendation.route"));
@@ -30,24 +33,26 @@ const corsOptions = {
     allowedHeaders: ['Authorization', 'Content-Type'],
 };
 
-
+// main server
 const http = require("http");
 const server = http.createServer(app);
 const socketIo = require("socket.io");
 const io = socketIo(server, { cors: corsOptions });
 
+// establish socket connections
 const { establishSocketConnections } = require("./sockets/baseOfConnections");
 establishSocketConnections(io);
 
+// test origin route 
 const path = require('path');
 app.get("/", (req, res) => {
     const photoPath = path.join(__dirname, './jok.jpg');
     res.sendFile(photoPath);
 });
 
+// server listening
 server.listen(process.env.PORT || 3000, process.env.LOCAL_HOST || "0.0.0.0", () => {
     console.log(`Server is up and runing on port ${process.env.PORT}!`)
 })
 
 module.exports = app;
-
