b690d7cc93a792f3e44bbc7101eb1a519cbdb552
ahmedabdelaziz5 <ahmedabdelaziz6019@gmail.com>
2023-11-23 00:20:28 +0200

    vido call sockets

--- sockets/baseOfConnections.js
+++ sockets/baseOfConnections.js
@@ -1,22 +1,29 @@
 const userModule = require('./modules/user.sockets');
 const matchModule = require('./modules/match.scokets');
+const videoCallModule = require('./modules/videoCall.sockets');
 
 //base socket connection with server
 exports.establishSocketConnections = (io) => {
 
     io.on('connection', (socket) => {
 
-        socket.on('joinUserRoom', (data, ack) => userModule.joinUserRoom(socket, data, ack));
+        // emit socket id to client
+        socket.emit("mySocket", socket.id);
 
+        // user Module events
+        socket.on('joinUserRoom', (data, ack) => userModule.joinUserRoom(socket, data, ack));
         socket.on('notifyUserRoom', (data, ack) => userModule.notifyUserRoom(socket, data, ack));
 
+        // match Module events
         socket.on('acceptPartnerRequest', (data, ack) => matchModule.acceptPartnerRequest(socket, data, ack));
-
         socket.on('disMatch', (data, ack) => matchModule.disMatch(io, socket, data, ack));
-
         socket.on('joinMatchRoom', (data, ack) => matchModule.joinMatchRoom(socket, data, ack));
-
         socket.on('sendDataToMatchRoom', (data, ack) => matchModule.sendDataToMatchRoom(socket, data, ack));
 
+        // videoCall Module events
+        socket.on("callUser", (data, ack) => videoCallModule.callUser(socket, data, ack));
+        socket.on("answerCall", (data, ack) => videoCallModule.answerCall(socket, data, ack));
+        socket.on("disconnect", (data, ack) => videoCallModule.disconnect(socket, data, ack));
+
     });
-}
+};
---
+++ sockets/modules/videoCall.sockets.js
@@ -0,0 +1,58 @@
+// events related to videoCall module
+
+// event is used to call user
+exports.callUser = (socket, callData, ack) => {
+    try {
+        socket.to(callData.userToCall).emit("callUser", callData);
+        ack({
+            success: true,
+            message: `user called successfully !`,
+        })
+    }
+    catch (err) {
+        console.log(err.message)
+        return ack({
+            success: false,
+            message: `error while calling user !`,
+        })
+    }
+
+};
+
+// event is used to answer call
+exports.answerCall = (socket, callData, ack) => {
+    try {
+        socket.to(callData.to).emit("callAccepted", callData);
+        ack({
+            success: true,
+            message: `call answered successfully !`,
+        })
+    }
+    catch (err) {
+        console.log(err.message)
+        return ack({
+            success: false,
+            message: `error while answering call !`,
+        })
+    }
+
+};
+
+// event is used to end video call
+exports.disconnect = (socket, data, ack) => {
+    try {
+        const roomId = socket.handshake.query.roomId;
+        socket.to(roomId).emit("callEnded");
+        ack({
+            success: true,
+            message: `call ended successfully !`,
+        })
+    }
+    catch (err) {
+        console.log(err.message)
+        return ack({
+            success: false,
+            message: `error while ending video call !`,
+        })
+    }
+};
