847e760d38be92b8c7b1568a85a10beffb57a903
ahmedabdelaziz5 <ahmedabdelaziz6019@gmail.com>
2024-02-29 18:04:31 +0200

    note module ( socket module )

--- sockets/modules/note.sockets.js
+++ sockets/modules/note.sockets.js
@@ -1,61 +1,127 @@
 // events related to note module 
+const { checkSocketAuth } = require('../Auth.js');
 
-exports.addNote = async (req, res) => {
-        try {
-
-        }
-        catch (err) {
-            return res.status(500).json({
-                message: "error",
-                error: err.message
+exports.addNote = async (socket, data, ack) => {
+    try {
+        const { token, matchId } = socket.handshake.query;
+        const isAuth = await checkSocketAuth(token, matchId);
+        if (!isAuth.success) {
+            return ack({
+                success: false,
+                message: "Not Authorized !"
             })
         }
-};
-
-exports.updateNote = async (req, res) => {
-    try {
-
+        socket.broadcast.to(matchId).emit("getNote", { data: data });
+        return ack({
+            success: true,
+            message: `note was added successfully !`,
+            data: data
+        })
     }
     catch (err) {
-        return res.status(500).json({
-            message: "error",
-            error: err.message
+        return ack({
+            success: false,
+            message: `error while adding note !`,
         })
     }
 };
 
-exports.pinNote = async (req, res) => {
+exports.updateNote = async (socket, data, ack) => {
     try {
-
+        const { token, matchId } = socket.handshake.query;
+        const isAuth = await checkSocketAuth(token, matchId);
+        if (!isAuth.success) {
+            return ack({
+                success: false,
+                message: "Not Authorized !"
+            })
+        }
+        socket.broadcast.to(matchId).emit("getUpdatedNote", { data: data });
+        return ack({
+            success: true,
+            message: `note was updated successfully !`,
+            data: data
+        })
     }
     catch (err) {
-        return res.status(500).json({
-            message: "error",
-            error: err.message
+        return ack({
+            success: false,
+            message: `error while updating note !`,
         })
     }
 };
 
-exports.deleteNote = async (req, res) => {
+exports.pinNote = async (socket, data, ack) => {
     try {
-
+        const { token, matchId } = socket.handshake.query;
+        const isAuth = await checkSocketAuth(token, matchId);
+        if (!isAuth.success) {
+            return ack({
+                success: false,
+                message: "Not Authorized !"
+            })
+        }
+        socket.broadcast.to(matchId).emit("notePinned", { data: data });
+        return ack({
+            success: true,
+            message: `note was pinned successfully !`,
+            data: data
+        })
     }
     catch (err) {
-        return res.status(500).json({
-            message: "error",
-            error: err.message
+        return ack({
+            success: false,
+            message: `error while pinning note !`,
         })
     }
 };
 
-exports.restorNote = async (req, res) => {
+exports.deleteNote = async (socket, data, ack) => {
     try {
-
+        const { token, matchId } = socket.handshake.query;
+        const isAuth = await checkSocketAuth(token, matchId);
+        if (!isAuth.success) {
+            return ack({
+                success: false,
+                message: "Not Authorized !"
+            })
+        }
+        socket.broadcast.to(matchId).emit("noteDeleted", { data: data });
+        return ack({
+            success: true,
+            message: `note was deleted successfully !`,
+            data: data
+        })
     }
     catch (err) {
-        return res.status(500).json({
-            message: "error",
-            error: err.message
+        return ack({
+            success: false,
+            message: `error while deleting note !`,
+        })
+    }
+};
+
+exports.restoreNote = async (socket, data, ack) => {
+    try {
+        const { token, matchId } = socket.handshake.query;
+        const isAuth = await checkSocketAuth(token, matchId);
+        if (!isAuth.success) {
+            return ack({
+                success: false,
+                message: "Not Authorized !"
+            })
+        }
+        socket.broadcast.to(matchId).emit("noteRestored", { data: data });
+        return ack({
+            success: true,
+            message: `note was restored successfully !`,
+            data: data
+        })
+    }
+    catch (err) {
+        return ack({
+            success: false,
+            message: `error while restoring note !`,
         })
     }
 };
